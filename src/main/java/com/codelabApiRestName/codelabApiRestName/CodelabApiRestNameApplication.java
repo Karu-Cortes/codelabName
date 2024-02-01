6741b8da4f775354a05c7210250a77e0541925fe
KarenCortes <karenj.cortesn@gmail.com>
2024-02-01 18:43:29 -0500

    rest api Name

---
+++ src/main/java/com/codelabApiRestName/codelabApiRestName/CodelabApiRestNameApplication.java
@@ -0,0 +1,13 @@
+package com.codelabApiRestName.codelabApiRestName;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodelabApiRestNameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodelabApiRestNameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codelabApiRestName/codelabApiRestName/CodelabApiRestNameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codelabApiRestName.codelabApiRestName;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodelabApiRestNameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
